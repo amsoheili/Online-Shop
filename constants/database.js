14919e4441b0b32930399947de1ade26e76ccbe1
amsoheili <amirmohammad.soheili1380@gmail.com>
2022-11-04 14:51:47 +0330

    some  building error resolved and mongo url changed to online

--- constants/database.js
+++ constants/database.js
@@ -1,5 +1,5 @@
 // local : "mongodb://127.0.0.1:27017/online-shop"
-// export const DATABASE_URI =
-//   "mongodb+srv://soheili:soheili@cluster0.gvxtnwo.mongodb.net/online-shop?retryWrites=true&w=majority";
+export const DATABASE_URI =
+  "mongodb+srv://soheili:soheili@cluster0.gvxtnwo.mongodb.net/online-shop?retryWrites=true&w=majority";
 
-export const DATABASE_URI = "mongodb://127.0.0.1:27017/online-shop";
+// export const DATABASE_URI = "mongodb://127.0.0.1:27017/online-shop";
